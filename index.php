a48a763848dc9fcba5bdebc261b6327b8f021321
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-03-17 01:35:36 +0700

    Custom Data Type

--- index.php
+++ index.php
@@ -1,54 +1,113 @@
 <html>
 <head>
 	<title>Presensi Cloud</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Nunito" />
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+	<!-- Nucleo Icons -->
+	<link href="./assets/css/nucleo-icons.css" rel="stylesheet" />
+	<link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+	<!-- Font Awesome Icons -->
+	<link href="./assets/css/font-awesome.css" rel="stylesheet" />
+	<link href="./assets/css/nucleo-svg.css" rel="stylesheet" />
+	<!-- CSS Files -->
+	<link href="./assets/css/argon-design-system.css?v=1.2.2" rel="stylesheet" />
 </head>
 <style>
-		p,b,u,a,i,h1,h2,h3,h4,h5,h6{
-			font-family: 'Roboto', sans-serif;
+		html,p,b,u,a,i,h1,h2,h3,h4,h5,h6{
+			font-family: 'Nunito', sans-serif;
+		}
+		body{
+			background: grey;
+		}
+		#customs{
+			margin : 2% 0%;
+		}
+		.form-control{
+			width:20%;
+			height:4%;
+			display:inline;
+			margin : 0% 1%;
+		}
+		.card{
+			margin :1% 4%;
+			padding: 2%;
+			border-radius: 1%;
+			background: white;
+			display:block;
+			height:95%;
+			box-shadow: 0 10px 10px 0 rgba(0,0,0,0.2);
 		}
 </style>
 <body>
-	<h2>Multi Schema Cloud</h2>
-	<h3 style="color: green; font-weight: bold">
+	<div class="card">
+		<h2>Multi Schema Cloud</h2>
 		<?php 
-		include('connectdb.php');
-		$mysqli = konek('localhost', 'root', '');
-		echo "Success... ".$mysqli->host_info."\n";
-		?>
-	</h3>
-	<form method="POST" enctype="multipart/form-data" action="simpan.php">
-		<label>Nama Universitas </label><input required type="text" name="nama"/><br><br>
-		<div id="customs">
-			<label>Custom Field </label><label>Entity : </label>
-			<select  name="entity[]">
-				<option value="fakultass">Fakultas</option>
-				<option value="jadwals">Jadwal</option>
-				<option value="jurusanss">Jurusan</option>
-				<option value="kehadirans">Kehadiran</option>
-				<option value="mahasiswas">Mahasiswa</option>
-				<option value="matakuliahs">Mata Kuliah</option>
-				<option value="matakuliahs_buka">Mata Kuliah Yang Buka</option>
-				<option value="matakuliahs_kp">Kelas Pararel Mata Kuliah</option>
-			</select>
-			<label> &nbsp; Field : </label><input required type="text" name="field[]"/>
-		</div>
-		<div style="margin-left: 25%;">
-			<input type="button" value="Add More Custom Field" id="addcustom"/><br>
-		</div>
-		<div style="margin-left: 15%;">
-			<input type="submit" value="Daftar"/>
-		</div>
-	</form>
-	<script type="text/javascript">
-		$("#addcustom").click(function(){
-			$("#customs").append("<div style='margin-left: 6%;''><label>Entity : </label><select  name='entity[]'><option value='fakultass'>Fakultas</option><option value='jurusanss'>Jurusan</option><option value='kehadirans'>Kehadiran</option><option value='mahasiswas'>Mahasiswa</option><option value='matakuliahs'>Mata Kuliah</option><option value='matakuliahs_buka'>Mata Kuliah Yang Buka</option><option value='matakuliahs_kp'>Kelas Pararel Mata Kuliah</option></select><label> Field: </label><input required type='text' name='field[]'/>&nbsp;<input type='button' value='Hapus' id='deletecustom'/></div>");
-		});
-		$('body').on('click', '#deletecustom', function(){
-			$(this).parent().remove();
-		});
-	</script>
+			include('connectdb.php');
+			$mysqli = konek('localhost', 'root', '');
+			if($mysqli->connect_errno) {
+				echo "<h3 style='color: red; font-weight: bold'>Error Connect... ".$mysqli->connect_errno."\n</h3>";
+				die();
+			}else{
+				echo "<h3 style='color: green; font-weight: bold'>Success... ".$mysqli->host_info."\n</h3>";
+			}
+			?>
+		<form method="POST" enctype="multipart/form-data" action="simpan.php">
+			<label>Nama Universitas : </label><input class="form-control" required type="text" name="nama"/><br><br>
+			<div id="customs">
+				<label>Custom Field </label><br><label>Nama : </label>
+				<select name="entity[]" selected="Fakultas">
+					<option value="fakultass">Fakultas</option>
+					<option value="jadwals">Jadwal</option>
+					<option value="jurusanss">Jurusan</option>
+					<option value="kehadirans">Kehadiran</option>
+					<option value="mahasiswas">Mahasiswa</option>
+					<option value="matakuliahs">Mata Kuliah</option>
+					<option value="matakuliahs_buka">Mata Kuliah Yang Buka</option>
+					<option value="matakuliahs_kp">Kelas Pararel Mata Kuliah</option>
+				</select>
+				<label> &nbsp; Field : </label><input class="form-control" required type="text" name="field[]"/>
+				<label> &nbsp; Tipe : </label>
+				<select id='datatipe' name="typee[]" selected="Varchar / String Text">
+					<option value="varchar(45)">Varchar / String Text</option>
+					<option value="int">Integer / Number</option>
+					<option value="datetime">Datetime</option>
+					<option value="double">Double / Decimal Number</option>
+				</select>
+			</div>
+			<div style="margin: -63px 0 0 67%;">
+				<input class="btn btn-sm btn-outline-default btn-round" type="button" value="Add More Custom Field" id="addcustom"/>
+				<input class="btn btn-sm btn-primary" type="submit" value="Daftar"/>
+			</div>
+		</form>
+		<script type="text/javascript">
+			$("#addcustom").click(function(){
+				$("#customs").append("<div><label>Nama :&nbsp;</label>"+
+				"<select  name='entity[]' selected='Fakultas'>"+
+					"<option value='fakultass'>Fakultas</option>"+
+					"<option value='jadwals'>Jadwal</option>"+
+					"<option value='jurusanss'>Jurusan</option>"+
+					"<option value='kehadirans'>Kehadiran</option>"+
+					"<option value='mahasiswas'>Mahasiswa</option>"+
+					"<option value='matakuliahs'>Mata Kuliah</option>"+
+					"<option value='matakuliahs_buka'>Mata Kuliah Yang Buka</option>"+
+					"<option value='matakuliahs_kp'>Kelas Pararel Mata Kuliah</option>"+
+				"</select>"+
+				"<label> &nbsp; &nbsp;Field :&nbsp; </label><input class='form-control' required type='text' name='field[]'/>"+
+				"<label> &nbsp; Tipe :&nbsp; </label>"+
+				"<select  name='typee[]' selected='Varchar / String Text'>"+
+					"<option value='varchar'>Varchar / String Text</option>"+
+					"<option value='int'>Integer / Number</option>"+
+					"<option value='datetime'>Datetime</option>"+
+					"<option value='double'>Double / Decimal Number</option>"+
+				"</select> &nbsp;<input class='btn btn-sm btn-danger' type='button' value='Hapus' id='deletecustom'/></div>");
+			});
+			$('body').on('click', '#deletecustom', function(){
+				$(this).parent().remove();
+			});
+		</script>
+	</div>
 </body>
 </html>
--- simpan.php
+++ simpan.php
@@ -5,24 +5,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nama = $_POST['nama'];
     $entities = $_POST['entity'];
     $fields = $_POST['field'];
-
+    $typee = $_POST['typee'];
+    for ($i=0; $i <= count($fields)-1 ; $i++) { 
+        $fields[$i] = strtolower(str_replace(" ", "_", $fields[$i]));
+    }
     if ($mysqli->connect_errno) {
         printf("Connect failed: %s\n", $mysqli->connect_error);
         exit();
     }
     else {
         $mysqli->select_db('presensi_cloud');
-        //simpan ke database
         $sql = "insert into universitass (nama) values ('".$nama."')";
         $result = $mysqli->query($sql);
         if ($result === TRUE) {
-            //jika berhasil simpan ke database, maka selanjutnya ambil id yang tersimpan
-            //dan buat schema dengan id tersebut
             $sql2 = "select id from universitass order by id desc limit 1";
             $result2 = $mysqli->query($sql2);
             $row = $result2->fetch_assoc();
             $id = $row['id'];
-            //create new schema
             $newSchema = 'presensi_cloud_'.$id;
             $sql3 = "create database ".$newSchema;
             $result3 = $mysqli->query($sql3);
@@ -32,20 +31,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username      = "root";
             $password      = "";
 
-            // $cmd = "mysql -h {$server_name} -u {$username} -p{$password} {$newSchema} < $restore_file";
             $cmd = "mysql -h $server_name -u $username $newSchema < $restore_file";
-            
             exec($cmd);
-
-            // echo print_r($entities)."<br>";
-            // echo print_r($fields)."<br>";
-
             $mysqli->select_db($newSchema);
             for ($i=0; $i <= count($entities)-1 ; $i++) { 
-                $sql = "ALTER TABLE ".$entities[$i]." ADD COLUMN ".$fields[$i]." VARCHAR(45)";
+                $sql = "ALTER TABLE ".$entities[$i]." ADD COLUMN ".$fields[$i]." ".$typee[$i];
                 $result = $mysqli->query($sql);
             }
-            
             $mysqli->select_db('presensi_cloud');
             $sql = "Insert Into metadatas (entity, custom_field, universitass_id) Values (?, ?,?)";
             for ($i=0; $i <= count($entities)-1 ; $i++) { 
@@ -53,7 +45,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $stmt->bind_param("ssi", $entities[$i], $fields[$i], $id);
                 $stmt->execute();
             }
-            
             header("Location:index.php");
             exit;
         }
