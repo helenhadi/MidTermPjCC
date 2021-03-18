001d07259533712eafa57916cd8407c1ce956d45
Jackly Christanto Perdana <62692463+jkl1721@users.noreply.github.com>
2021-03-18 14:35:15 +0700

    New SQL n Database

--- presensi_cloud.sql
+++ presensi_cloud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 16, 2021 at 10:26 PM
+-- Generation Time: Mar 18, 2021 at 08:34 AM
 -- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.27
+-- PHP Version: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `fakultass`
+--
+
+CREATE TABLE `fakultass` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `fakultass`
+--
+
+INSERT INTO `fakultass` (`id`, `nama`) VALUES
+(1, 'Kedokteran'),
+(2, 'Industri Kreatif'),
+(3, 'Hukum'),
+(4, 'Psikologi'),
+(5, 'Bisnis dan Ekonomika'),
+(6, 'Teknik'),
+(7, 'Farmasi'),
+(8, 'Teknobio');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `metadatas`
 --
 
@@ -31,67 +56,41 @@ CREATE TABLE `metadatas` (
   `id` int(11) NOT NULL,
   `entity` varchar(45) NOT NULL,
   `custom_field` varchar(45) NOT NULL,
-  `universitass_id` int(11) NOT NULL
+  `fakultas_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `metadatas`
---
-
-INSERT INTO `metadatas` (`id`, `entity`, `custom_field`, `universitass_id`) VALUES
-(1, 'fakultass', 'custom1', 1),
-(2, 'mahasiswas', 'customo2', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `universitass`
---
-
-CREATE TABLE `universitass` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `universitass`
---
-
-INSERT INTO `universitass` (`id`, `nama`) VALUES
-(1, 'universitasnya');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `fakultass`
+--
+ALTER TABLE `fakultass`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `metadatas`
 --
 ALTER TABLE `metadatas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_metadatas_universitass1_idx` (`universitass_id`);
-
---
--- Indexes for table `universitass`
---
-ALTER TABLE `universitass`
-  ADD PRIMARY KEY (`id`);
+  ADD KEY `fk_metadatas_universitass1_idx` (`fakultas_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `fakultass`
+--
+ALTER TABLE `fakultass`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `metadatas`
 --
 ALTER TABLE `metadatas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `universitass`
---
-ALTER TABLE `universitass`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -101,7 +100,7 @@ ALTER TABLE `universitass`
 -- Constraints for table `metadatas`
 --
 ALTER TABLE `metadatas`
-  ADD CONSTRAINT `fk_metadatas_universitass1` FOREIGN KEY (`universitass_id`) REFERENCES `universitass` (`id`);
+  ADD CONSTRAINT `fk_metadatas_universitass1` FOREIGN KEY (`fakultas_id`) REFERENCES `fakultass` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
