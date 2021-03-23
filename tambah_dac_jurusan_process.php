4216db28fc0f2a74f501a409ef7a091ae1489751
Helen Hadi <69718814+helenhadi@users.noreply.github.com>
2021-03-23 10:29:23 +0700

    kebali tadi bund :v:

--- tambah_dac_jurusan_process.php
+++ tambah_dac_jurusan_process.php
@@ -2,47 +2,29 @@
 session_start();
 include ('connectdb.php');
 $mysqli = konek('localhost', 'root', '', '');
-$mysqli->select_db('presensi_cloud');
-if (isset($_POST['adduser'])) {
-    if ($_POST['password'] == $_POST['r_password']) {
-        $nama = $_POST['nama'];
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $saltedPwd = sha1(md5($password));
-        $jabatan = $_POST['jabatan'];
-        $fakultas = $_POST['fakultas'];
-        $jurusan = $_POST['jurusan'];
+$database = ""; //MASIH BINGUNG HOW TO GET THE JURUSAN ID :)
+$mysqli->select_db($database);
+if (isset($_POST['adddacj'])) {
+  $kode = $_POST['kode'];
+  $entity = $_POST['entity'];
+  $field = $_POST['field'];
+  $operator = $_POST['operator'];
+  $value = $_POST['value'];
 
-        if ($jabatan == 'dekan' || $jabatan == 'wadek') {
-            $jurusan = 0;
-        }
+  $sql = "insert into dac_rules(kode, entity, field, operator, value) values (?, ?, ?, ?, ?, ?)";
+  $stmt = $mysqli->prepare($sql);
+  $stmt->bind_param("ssssss", $kode, $entity, $field, $operator, $value);
+  $stmt->execute();
 
-        if ($jurusan == 0) {
-            $sql = "insert into users(nama, username, password, jabatan, fakultass_id) values (?, ?, ?, ?, ?)";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("ssssi", $nama, $username, $saltedPwd, $jabatan, $fakultas);
-        }
-        else {
-            $sql = "insert into users(nama, username, password, jabatan, fakultass_id, jurusans_id) values (?, ?, ?, ?, ?, ?)";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("ssssii", $nama, $username, $saltedPwd, $jabatan, $fakultas, $jurusan);
-        }
-        
-        $stmt->execute();
+  if ($stmt->affected_rows > 0) {
+    $_SESSION['success'] = "Insert DAC succeed!";
+  }
+  else {
+    $_SESSION['error'] = "Failed to insert DAC, please try again...";
+  }
 
-        if ($stmt->affected_rows > 0) {
-            $_SESSION['success'] = "Insert user succeed!";
-        }
-        else {
-            $_SESSION['error'] = "Failed to insert user, please try again...";
-        }
-    }
-    else {
-        $_SESSION['error'] = "Passwords does not match!";
-    }
-
-    header("Location: tambah_user.php");
+  header("Location: tambah_dac_fakultas.php");
 }
 else
-    header("Location: dashboard.php");
+  header("Location: dashboard.php");
 ?>
--- tambah_jurusan_proses.php
+++ tambah_jurusan_proses.php
@@ -2,29 +2,47 @@
 session_start();
 include ('connectdb.php');
 $mysqli = konek('localhost', 'root', '', '');
-$database = ""; //MASIH BINGUNG HOW TO GET THE JURUSAN ID :)
-$mysqli->select_db($database);
-if (isset($_POST['adddacj'])) {
-  $kode = $_POST['kode'];
-  $entity = $_POST['entity'];
-  $field = $_POST['field'];
-  $operator = $_POST['operator'];
-  $value = $_POST['value'];
+$mysqli->select_db('presensi_cloud');
+if (isset($_POST['adduser'])) {
+    if ($_POST['password'] == $_POST['r_password']) {
+        $nama = $_POST['nama'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $saltedPwd = sha1(md5($password));
+        $jabatan = $_POST['jabatan'];
+        $fakultas = $_POST['fakultas'];
+        $jurusan = $_POST['jurusan'];
 
-  $sql = "insert into dac_rules(kode, entity, field, operator, value) values (?, ?, ?, ?, ?, ?)";
-  $stmt = $mysqli->prepare($sql);
-  $stmt->bind_param("ssssss", $kode, $entity, $field, $operator, $value);
-  $stmt->execute();
+        if ($jabatan == 'dekan' || $jabatan == 'wadek') {
+            $jurusan = 0;
+        }
 
-  if ($stmt->affected_rows > 0) {
-    $_SESSION['success'] = "Insert DAC succeed!";
-  }
-  else {
-    $_SESSION['error'] = "Failed to insert DAC, please try again...";
-  }
+        if ($jurusan == 0) {
+            $sql = "insert into users(nama, username, password, jabatan, fakultass_id) values (?, ?, ?, ?, ?)";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("ssssi", $nama, $username, $saltedPwd, $jabatan, $fakultas);
+        }
+        else {
+            $sql = "insert into users(nama, username, password, jabatan, fakultass_id, jurusans_id) values (?, ?, ?, ?, ?, ?)";
+            $stmt = $mysqli->prepare($sql);
+            $stmt->bind_param("ssssii", $nama, $username, $saltedPwd, $jabatan, $fakultas, $jurusan);
+        }
+        
+        $stmt->execute();
 
-  header("Location: tambah_dac_fakultas.php");
+        if ($stmt->affected_rows > 0) {
+            $_SESSION['success'] = "Insert user succeed!";
+        }
+        else {
+            $_SESSION['error'] = "Failed to insert user, please try again...";
+        }
+    }
+    else {
+        $_SESSION['error'] = "Passwords does not match!";
+    }
+
+    header("Location: tambah_user.php");
 }
 else
-  header("Location: dashboard.php");
+    header("Location: dashboard.php");
 ?>
